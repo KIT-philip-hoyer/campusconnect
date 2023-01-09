572cd2b9e14b9dd70c54809c0f92a493ac75271e
philip.hoyer <hoyer@kit.edu>
2023-01-09 15:47:25 +0100

    batch for buiding installation files

---
+++ buildZipFiles.bat
@@ -0,0 +1,6 @@
+cd %~dp0
+"%ProgramFiles%\7-Zip\7z.exe" a .\campusconnect-auth.zip .\auth\campusconnect
+"%ProgramFiles%\7-Zip\7z.exe" a .\campusconnect-blocks.zip .\blocks\campusconnect
+"%ProgramFiles%\7-Zip\7z.exe" a .\campusconnect-enrol.zip .\enrol\campusconnect
+"%ProgramFiles%\7-Zip\7z.exe" a .\campusconnect-local.zip .\local\campusconnect
+pause
